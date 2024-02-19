fe1c2b3bd5f029d103a2cb611c51abda2e65b84e
George Daian <georgedaianx@gmail.com>
2024-02-20 00:44:09 +0200

    Wireup stdout & stderr to parent process

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
+use std::process::Stdio;
+
 use anyhow::{Context, Result};
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
-
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
     let output = std::process::Command::new(command)
         .args(command_args)
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
         .output()
         .with_context(|| {
             format!(
@@ -15,11 +18,13 @@ fn main() -> Result<()> {
                 command, command_args
             )
         })?;
-    
+
     if output.status.success() {
         let std_out = std::str::from_utf8(&output.stdout)?;
-        println!("{}", std_out);
+        print!("{}", std_out);
     } else {
+        let std_err = std::str::from_utf8(&output.stderr)?;
+        print!("{}", std_err);
         std::process::exit(1);
     }
 
